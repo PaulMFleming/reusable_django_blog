6d25feeaa0d6932a643ba1e38f3fca216868b508
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-09-23 15:16:43 +0100

    updated urls.py

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^/$', views.post_list),
-    url(r'^/(?P<id>\d+)/$', views.post_detail),
+    url(r'^$', views.post_list),
+    url(r'^(?P<id>\d+)/$', views.post_detail),
     url(r'^post/new/$', views.new_post, name='new_post'),
-    url(r'^/(?P<id>\d+)/edit$', views.edit_post, name='edit'),
+    url(r'^(?P<id>\d+)/edit$', views.edit_post, name='edit'),
 ]
